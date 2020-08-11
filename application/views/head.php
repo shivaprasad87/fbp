96b8a87792be8b9747e9bd8009773ae3e467f037
shiva prasad <shiva@secondsdigital.com>
2020-08-11 15:34:05 +0530

    og tags width

--- application/views/head.php
+++ application/views/head.php
@@ -14,8 +14,8 @@
         <meta property="og:description" content="<?= $description ?>">
         <meta property="og:type" content="website">
         <meta property="og:image" content="<?= base_url('assets/img/logo.png'); ?>">
-        <meta property="og:image:width" content="400" />
-        <meta property="og:image:height" content="300" />
+        <meta property="og:image:width" content="250" />
+        <meta property="og:image:height" content="200" />
         <meta name="twitter:card" content="summary">
         <meta name="twitter:site" content="@FBPTweets">
         <meta name="twitter:title" content="<?= $title ?>">
